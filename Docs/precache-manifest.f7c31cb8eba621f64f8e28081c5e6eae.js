145d23b20218b3796f1d62c80e287380723376cc
Alejandro Soto Garcia <alejandro.soto@iMac-de-web.local>
2020-08-27 16:28:11 -0500

    despliegue de la carepeta build 👨🏽‍💻

---
+++ Docs/precache-manifest.f7c31cb8eba621f64f8e28081c5e6eae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69fc00e4496dbfe8dedf39b259e9276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e93450e6468e9924157",
+    "url": "/static/css/main.eddaf542.chunk.css"
+  },
+  {
+    "revision": "aa8b4ec0d42a8d346279",
+    "url": "/static/js/2.e273df15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e273df15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e93450e6468e9924157",
+    "url": "/static/js/main.899eb7f5.chunk.js"
+  },
+  {
+    "revision": "0cfa452900319396423f",
+    "url": "/static/js/runtime-main.9c54e535.js"
+  }
+]);
